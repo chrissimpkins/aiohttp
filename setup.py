6fb78f78106e223365df4c542750f7142a99997e
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-09-08 00:13:11 +0300

    Drop python 3.5

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ from distutils.errors import (CCompilerError, DistutilsExecError,
 from setuptools import Extension, setup
 
 
-if sys.version_info < (3, 5, 3):
-    raise RuntimeError("aiohttp 3.x requires Python 3.5.3+")
+if sys.version_info < (3, 6):
+    raise RuntimeError("aiohttp 3.x requires Python 3.6+")
 
 
 NO_EXTENSIONS = bool(os.environ.get('AIOHTTP_NO_EXTENSIONS'))  # type: bool
@@ -130,7 +130,7 @@ args = dict(
     },
     license='Apache 2',
     packages=['aiohttp'],
-    python_requires='>=3.5.3',
+    python_requires='>=3.6',
     install_requires=install_requires,
     extras_require={
         'speedups': [
